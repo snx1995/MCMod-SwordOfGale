8dc568c30e146f8fc263f94b9c376d556fca7b94
banyq <snx1788@gmail.com>
2019-02-19 20:47:29 +0800

    event

--- src/main/java/top/byq/swordofgale/block/GaleStoneBlock.java
+++ src/main/java/top/byq/swordofgale/block/GaleStoneBlock.java
@@ -2,11 +2,14 @@ package top.byq.swordofgale.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
@@ -37,7 +40,8 @@ public class GaleStoneBlock extends GaleBlock {
         playerIn.sendMessage(new TextComponentString("Gale stone at (" + pos.getX() + "," + pos.getY() + "," + pos.getZ()));
     }
 
-
-
-
+    @Override
+    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+        return super.onBlockActivated(worldIn, pos, state, playerIn, hand, facing, hitX, hitY, hitZ);
+    }
 }
